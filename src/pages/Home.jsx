2741fb60b75fa4d098c934e55dc3cf289a3db7fa
meitarg <meitarg@checkpoint.com>
2023-10-01 23:42:08 +0300

    home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,15 @@ import { NavLink } from "react-router-dom"
 
 export function Home() {
     return (
-        <section className="home">
-            <p>Home</p>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/mail/inbox">Mails</NavLink>
-        </section>
+        <div className="home">
+            <section className="home-main">
+                <h1>Home</h1>
+                <nav>
+                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="/about">About</NavLink>
+                    <NavLink to="/mail/inbox">Mails</NavLink>
+                </nav>
+            </section>
+        </div>
     )
 }
